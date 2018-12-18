03830c570b8b06ab4054dba7957e33f1633855c3
luizLHS <henriquesaydt2@hotmail.com>
2018-12-18 11:02:11 -0200

    leitura bit a bit do arquivo

--- main.c
+++ main.c
@@ -1,21 +1,48 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include "arvore.h"
 
 int main(int argc, char const *argv[])
 {
     FILE *arquivo;
     arquivo = fopen("teste.txt", "r");
     char b;
+    int freq[256];
+    //zera todo o vetor freq[256]
+    memset(freq, 0, sizeof(freq));
 
     while(fread(&b, 1, 1, arquivo)){
-        printf("Byte lido: %c\n", b);
-        printf("Bits:\n");
+        printf("\nByte lido: %c\n", b);
+        freq[b]++;
+
+        //Quebrando o Byte em bits
+        printf("Bits:");
         int i;
         for(i=0; i<8; i++){
-            int bit = (b >> i)&1;
-            printf("%d\n", bit);
+            char bit = (b >> i)&1;
+            printf("%d", bit);
         }
+        printf("\n");
+    }
+
+    arvore_l *tabela = freq_ordenar(freq);
+    
+    arvore_l *no_1;
+    arvore_l *no_2;
+    arvore_l *no_res = (arvore_l *)malloc(sizeof(arvore_l));
+    while(lista_tamanho > 1){
+        no_res->freq = no_1->freq + no_2->freq;
+        no_res->left = tabela;
+        no_res->right = tabela->prox;
+        no_res->data = NULL;
+        no_res->prox = lista_inserir(tabela, no_res->freq, NULL);
+        no_1 == tabela;
+        no_2 == tabela->prox;
+        lista_remover(tabela);
+
     }
     
+
     return 0;
 }
